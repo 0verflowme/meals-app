bf48c9ed05e1e0b1f3a33e9d8d037152a892f0ec
0verflowme <priyanshukumarpu@gmail.com>
2022-10-05 10:24:42 +0530

    bug fix

--- liked.js
+++ liked.js
@@ -1,26 +1,25 @@
 // const likedList = ["52975","52962"];
 if (localStorage.likedList) {
-  const likedList = JSON.parse(localStorage.likedList).reverse();
+  const likedList = JSON.parse(localStorage.likedList);
 
-  const maindiv = document.getElementById("liked-disp");
+  const mainDiv = document.getElementById("liked-disp");
 
   function showMealDetailsDiv(data) {
     console.log(data);
     data = data.meals[0];
     const img = data.strMealThumb;
     const text = data.strMeal;
-    const newd = document.createElement("div");
-    newd.classList.add("d-inline-flex");
-    newd.innerHTML = `
+    const newD = document.createElement("div");
+    newD.classList.add("d-inline-flex");
+    newD.innerHTML = `
       <div class="card d-inline-block m-3" style="width: 18rem;">
       <img src=${img} class="card-img-top" alt="...">
       <div class="card-body">
-          <p class="card-text">${text}
-              content.</p>
+          <p class="card-text text-center fw-light">${text}</p>
       </div>
   </div>
       `;
-    maindiv.appendChild(newd);
+    mainDiv.appendChild(newD);
   }
 
   for (var i of likedList) {
